869633ca01b016a93e62aa0cb921f4c84dcc186f
TonyMx <consultormid@yahoo.com>
2024-05-08 18:34:36 -0600

    adding soltions part I

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
+import operator
+
+
+class SimpleCalculator:
+    def add(self,*args):
+        return sum(args)
+    
+    def sub(self, a, b):
+        return a - b
+    
+    def mul(self, *args):
+        return reduce(operator.mul, args)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from simple_calculator.main import SimpleCalculator
 
+def test_add_two_numbers():
+    calculator = SimpleCalculator()
+
+    result=calculator.add(4,5)
+
+    assert result==9
+
+def test_add_three_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.add(4, 5, 6)
+
+    assert result == 15
+
+def test_add_many_numbers():
+    numbers = range(100)
+
+    calculator = SimpleCalculator()
+
+    result = calculator.add(*numbers)
+
+    assert result == 4950
+
+def test_subtract_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.sub(10, 3)
+
+    assert result == 7
+
+def test_mul_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.mul(6, 4)
+
+    assert result == 24
+
+def test_mul_many_numbers():
+    numbers = range(1, 10)
+
+    calculator = SimpleCalculator()
+
+    result = calculator.mul(*numbers)
+
+    assert result == 362880
