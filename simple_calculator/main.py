f83d0ec73cb56c8fe8012f3836f12b4947f5eb3a
Tony Diaz <info@tonydiaz.pro>
2024-05-09 00:07:59 -0600

    finish part II

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -11,4 +11,31 @@ class SimpleCalculator:
         return a - b
     
     def mul(self, *args):
+        if not all(args):
+            raise ValueError
+        
         return reduce(operator.mul, args)
+    
+    def div(self,a,b):
+        try:
+            return a / b
+        except ZeroDivisionError:
+            return float('inf')
+        
+    def avg(self,lst: list,ut: int =0,lt:int=0):
+        return_value=0
+
+        if len(lst)>0:
+            if ut>0:
+               lst = list(filter(lambda x: x<=ut,lst))
+            
+            if lt>0:
+               lst= list(filter(lambda x: x>=lt,lst))
+
+            count= len(lst)
+
+            if count > 0:
+               total_sum = reduce((lambda x,y:x+y),lst)
+               return_value = round(float(total_sum/count),2)
+        
+        return return_value
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pytest
 from simple_calculator.main import SimpleCalculator
 
 def test_add_two_numbers():
@@ -47,3 +48,81 @@ def test_mul_many_numbers():
     result = calculator.mul(*numbers)
 
     assert result == 362880
+
+def test_div_two_numbers_float():
+    calculator= SimpleCalculator()
+
+    result = calculator.div(1,2)
+
+    assert result==0.5
+
+def test_div_by_zero_returns_inf():
+    calculator = SimpleCalculator()
+
+    result = calculator.div(5, 0)
+
+    assert result == float('inf')
+
+def test_mul_by_zero_raises_exception():
+    calculator = SimpleCalculator()
+
+    with pytest.raises(ValueError):
+        calculator.mul(3, 0)
+
+#my test for part II
+def test_compute_avg_iterable():
+    calculator= SimpleCalculator()
+
+    result=calculator.avg([2,5,12,98])
+
+    assert result == 29.25
+
+def test_compute_avg_iterable_optional_ut():
+    calculator= SimpleCalculator()
+
+    result = calculator.avg([2,5,12,98],90)
+
+    assert result == 6.33
+
+def test_compute_avg_iterable_optional_lt():
+    calculator= SimpleCalculator()
+
+    result = calculator.avg([2,5,12,15,98],90,11)
+
+    assert result==13.5
+
+def test_compute_avg_iterable_optional_ut_not_included():
+    calculator= SimpleCalculator()
+
+    result = calculator.avg([2,5,12,15,98],12)
+
+    assert result == 6.33
+
+def test_compute_avg_iterable_optional_lt_not_included():
+    calculator= SimpleCalculator()
+
+    result = calculator.avg([2,5,12,15,98],lt=15)
+
+    assert result == 56.5
+
+def test_compute_avg_iterable_empty():
+    calculator= SimpleCalculator()
+
+    result = calculator.avg([])
+
+    assert result == 0
+
+def test_compute_avg_iterable_after_oulier_removal():
+    calculator= SimpleCalculator()
+
+    result = calculator.avg([12,98],ut=90,lt=15)
+
+    assert result == 0
+
+def test_compute_avg_iterable_empty_oulier_removal():
+    #probablemente esta prueba esta de mas...
+    calculator= SimpleCalculator()
+
+    result = calculator.avg([],ut=90,lt=15)
+
+    assert result == 0
